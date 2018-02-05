4b19937fee35f83915b0ac34bbe202c1da38246b
Falborski Artur <falborski.artur@gmail.com>
2018-02-05 23:07:47 +0100

    added resources reducer

--- src/state/parts/resources.js
+++ src/state/parts/resources.js
@@ -1,8 +1,37 @@
 const initialState = {
+  gold: 0,
+  coal: 0,
+  iron: 0,
+  stone: 0,
+  wood: 0,
+  food: 0
 }
+
+const isNumber = val => (!isNaN(val) && typeof val === 'number' && isFinite(val))
+
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case 'INCREMENT': {
+      return {
+        gold: isNumber(action.gold) ? state.gold + action.gold : state.gold,
+        coal: isNumber(action.coal) ? state.coal + action.coal : state.coal,
+        iron: isNumber(action.iron) ? state.iron + action.iron : state.iron,
+        stone: isNumber(action.stone) ? state.stone + action.stone : state.stone,
+        wood: isNumber(action.wood) ? state.wood + action.wood : state.wood,
+        food: isNumber(action.food) ? state.food + action.food : state.food
+      }
+    }
+    case 'DECREMENT': {
+      return {
+        gold: isNumber(action.gold) ? state.gold - action.gold : state.gold,
+        coal: isNumber(action.coal) ? state.coal - action.coal : state.coal,
+        iron: isNumber(action.iron) ? state.iron - action.iron : state.iron,
+        stone: isNumber(action.stone) ? state.stone - action.stone : state.stone,
+        wood: isNumber(action.wood) ? state.wood - action.wood : state.wood,
+        food: isNumber(action.food) ? state.food - action.food : state.food
+      }
+    }
     default:
       return {
         ...state
