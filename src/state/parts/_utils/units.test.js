6f67b0927119a54e01d4695763d2047e4097595a
Artur Falborski <falborski.artur@gmail.com>
2018-02-09 23:04:15 +0100

    extended units reducer test with workers

--- src/state/parts/_utils/units.test.js
+++ src/state/parts/_utils/units.test.js
@@ -3,7 +3,13 @@ import units from '../units'
 const initialState = {
   fetching: null,
   error: null,
-  data: null
+  data: null,
+  workers: {
+    miners: 0,
+    quarriers: 0,
+    lumberjacks: 0,
+    farmers: 0,
+  }
 }
 
 describe('units', () => {
@@ -39,6 +45,7 @@ describe('units', () => {
     };
 
     const expectedState = {
+      ...initialState,
       fetching: false,
       error: null,
       data: {}
@@ -57,10 +64,31 @@ describe('units', () => {
     };
 
     const expectedState = {
+      ...initialState,
       fetching: false,
       error: 'Malformed JSON!',
       data: null
     };
     expect(units(testState, action)).toEqual(expectedState);
   });
+  it('should add worker of specific type', () => {
+    const actions = [
+      {type: 'ADD_WORKER', worker: 'miner'},
+      {type: 'ADD_WORKER', worker: 'quarrier'},
+      {type: 'ADD_WORKER', worker: 'lumberjack'},
+      {type: 'ADD_WORKER', worker: 'farmer'}
+    ];
+
+    const expectedStates = [
+      {...initialState, workers: {...initialState.workers, miners: 1}},
+      {...initialState, workers: {...initialState.workers, quarriers: 1}},
+      {...initialState, workers: {...initialState.workers, lumberjacks: 1}},
+      {...initialState, workers: {...initialState.workers, farmers: 1}}
+    ];
+
+    expect(units(initialState, actions[0])).toEqual(expectedStates[0]);
+    expect(units(initialState, actions[1])).toEqual(expectedStates[1]);
+    expect(units(initialState, actions[2])).toEqual(expectedStates[2]);
+    expect(units(initialState, actions[3])).toEqual(expectedStates[3]);
+  })
 });
