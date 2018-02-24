f41d7f057d027f907f0ea6581e56d6435b6f4125
Artur Falborski <falborski.artur@gmail.com>
2018-02-24 11:37:21 +0100

    import reducers to reducer.js

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,6 +1,11 @@
 import resources from './parts/resources'
+import units from './parts/units'
+import buildings from './parts/buildings'
+
 export default (state = {}, action) => {
   return {
-    resources: resources(state.resources, action)
+    resources: resources(state.resources, action),
+    units: units(state.units, action),
+    buildings: buildings(state.buildings, action)
   }
 }
