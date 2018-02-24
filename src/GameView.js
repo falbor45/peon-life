d8a7b0c5d195d546936029e7d2d1dd88421f1c28
Artur Falborski <falborski.artur@gmail.com>
2018-02-24 11:50:15 +0100

    fetch json files in GameView component

--- src/GameView.js
+++ src/GameView.js
@@ -1,7 +1,64 @@
 import React, {Component} from 'react'
+import { connect } from 'react-redux'
 import Swipe from 'react-easy-swipe'
 import ResourcesBar from './ResourcesView'
-export default class GameView extends Component {
+
+let mapStateToProps = state => {
+  return {
+    units: state.units,
+    buildings: state.buildings
+  }
+};
+
+let mapDipsatchToProps = dispatch => {
+  return {
+    unitsFetchBegin: () => dispatch({type: 'units/FETCH_BEGIN'}),
+    unitsFetchFail: () => dispatch({type: 'units/FETCH_FAIL'}),
+    unitsFetchSuccess: () => dispatch({type: 'units/FETCH_SUCCESS'}),
+    unitsSetSettings: data => dispatch({type: 'units/SET_SETTINGS', data}),
+    buildingsFetchBegin: () => dispatch({type: 'buildings/FETCH_BEGIN'}),
+    buildingsFetchFail: () => dispatch({type: 'buildings/FETCH_FAIL'}),
+    buildingsFetchSuccess: () => dispatch({type: 'buildings/FETCH_SUCCESS'}),
+    buildingsSetSettings: data => dispatch({type: 'buildings/SET_SETTINGS', data})
+  }
+}
+
+class GameView extends Component {
+
+  componentWillMount() {
+    this.props.unitsFetchBegin()
+    fetch(
+      `${process.env.PUBLIC_URL}/data/units.json`
+    ).then(
+      response => response.json().then(
+        data => {
+          this.props.unitsFetchSuccess(data);
+          this.props.unitsSetSettings(data);
+        }
+      ).catch(
+        error => this.props.unitsFetchFail('Malformed JSON!')
+      )
+    ).catch(
+      error => this.props.unitsFetchFail('Connection error!')
+    )
+
+    this.props.buildingsFetchBegin()
+    fetch(
+      `${process.env.PUBLIC_URL}/data/buildings.json`
+    ).then(
+      response => response.json().then(
+        data => {
+          this.props.buildingsFetchSuccess(data);
+          this.props.buildingsSetSettings(data);
+        }
+      ).catch(
+        error => this.props.buildingsFetchFail('Malformed JSON!')
+      )
+    ).catch(
+      error => this.props.buildingsFetchFail('Connection error!')
+    )
+  }
+
 
   constructor(props) {
     super(props);
@@ -53,3 +110,5 @@ export default class GameView extends Component {
     )
   }
 }
+
+export default connect(mapStateToProps, mapDipsatchToProps)(GameView)
