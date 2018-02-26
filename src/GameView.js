f0fe662d1a5b762b1c601a7b4076abf1ebb70adb
Artur Falborski <falborski.artur@gmail.com>
2018-02-26 13:03:13 +0100

    show loader while fetching data

--- src/GameView.js
+++ src/GameView.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import Swipe from 'react-easy-swipe'
 import ResourcesBar from './ResourcesView'
+import Loader from './Loader'
 
 let mapStateToProps = state => {
   return {
@@ -13,12 +14,12 @@ let mapStateToProps = state => {
 let mapDipsatchToProps = dispatch => {
   return {
     unitsFetchBegin: () => dispatch({type: 'units/FETCH_BEGIN'}),
-    unitsFetchFail: () => dispatch({type: 'units/FETCH_FAIL'}),
-    unitsFetchSuccess: () => dispatch({type: 'units/FETCH_SUCCESS'}),
+    unitsFetchFail: error => dispatch({type: 'units/FETCH_FAIL', error}),
+    unitsFetchSuccess: data => dispatch({type: 'units/FETCH_SUCCESS', data}),
     unitsSetSettings: data => dispatch({type: 'units/SET_SETTINGS', data}),
     buildingsFetchBegin: () => dispatch({type: 'buildings/FETCH_BEGIN'}),
-    buildingsFetchFail: () => dispatch({type: 'buildings/FETCH_FAIL'}),
-    buildingsFetchSuccess: () => dispatch({type: 'buildings/FETCH_SUCCESS'}),
+    buildingsFetchFail: error => dispatch({type: 'buildings/FETCH_FAIL', error}),
+    buildingsFetchSuccess: data => dispatch({type: 'buildings/FETCH_SUCCESS', data}),
     buildingsSetSettings: data => dispatch({type: 'buildings/SET_SETTINGS', data})
   }
 }
@@ -76,14 +77,12 @@ class GameView extends Component {
 
   onSwipeMove(position, event) {
     if (position.x - position.y > 150 && this.state.wasComponentSwiped === false) {
-      console.log('prawo')
       this.setState({
         viewedComponent: this.state.viewedComponent + 1 > 2 ? this.state.viewedComponent : this.state.viewedComponent + 1,
         wasComponentSwiped: true
       })
     }
     if (position.x - position.y < -150 && this.state.wasComponentSwiped === false) {
-      console.log('left')
       this.setState({
         viewedComponent: this.state.viewedComponent - 1 < 0 ? this.state.viewedComponent : this.state.viewedComponent - 1,
         wasComponentSwiped: true
@@ -100,12 +99,17 @@ class GameView extends Component {
   render() {
     return (
       <div>
-        <ResourcesBar/>
-        <Swipe
-          onSwipeStart={this.onSwipeStart.bind(this)}
-          onSwipeMove={this.onSwipeMove.bind(this)}
-          onSwipeEnd={this.onSwipeEnd.bind(this)}>
-        </Swipe>
+        {this.props.units.data !== null && false && this.props.buildings.data !== null ? (
+          <div>
+            <ResourcesBar/>
+            <Swipe
+              onSwipeStart={this.onSwipeStart.bind(this)}
+              onSwipeMove={this.onSwipeMove.bind(this)}
+              onSwipeEnd={this.onSwipeEnd.bind(this)}>
+            </Swipe>
+          </div>
+        ) : <Loader/>
+        }
       </div>
     )
   }
