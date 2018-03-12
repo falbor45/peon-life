cc0b51e1411e0de1d6ca2b3d1ce50dad695dd0e7
Artur Falborski <falborski.artur@gmail.com>
2018-03-12 09:57:45 +0100

    updated ResourcesView component

--- src/ResourcesView.js
+++ src/ResourcesView.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
+import 'normalize.css'
 import './ResourcesView.css'
 
 let mapStateToProps = state => {
@@ -11,19 +12,9 @@ let mapStateToProps = state => {
 class ResourcesView extends Component {
   render() {
     return (
-      <div className="resources__wrapper">
-      <div className="resources">
-        <div className="resources__row1">
-          <span>Coal: {this.props.resources.coal}</span>
-          <span>Wood: {this.props.resources.wood}</span>
-          <span>Stone: {this.props.resources.stone}</span>
-        </div>
-        <div className="resources__row2">
-          <span>Iron: {this.props.resources.iron}</span>
-          <span>Food: {this.props.resources.food}</span>
-          <span>Gold: {this.props.resources.gold}</span>
-        </div>
-      </div>
+      <div>
+        <p className="village-name">Peon's village</p>
+        <p className="gold-counter">{this.props.resources.gold} gold</p>
       </div>
     )
   }
