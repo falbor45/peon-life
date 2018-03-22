7db3ac092e9fc1e38ae6cb6b2e5e3788dbac4b3e
Artur Falborski <falborski.artur@gmail.com>
2018-03-22 18:16:15 +0100

    updated increments reducer

--- src/state/parts/increments.js
+++ src/state/parts/increments.js
@@ -1,22 +1,7 @@
 const initialState = {
   goldBase: 0,
   goldMulti: 1,
-  goldIncr: 0,
-  coalBase: 0,
-  coalMulti: 1,
-  coalIncr: 0,
-  ironBase: 0,
-  ironMulti: 1,
-  ironIncr: 0,
-  stoneBase: 0,
-  stoneMulti: 1,
-  stoneIncr: 0,
-  woodBase: 0,
-  woodMulti: 1,
-  woodIncr: 0,
-  foodBase: 0,
-  foodMulti: 1,
-  foodIncr: 0
+  goldIncr: 0
 }
 
 const isNumber = val => (!isNaN(val) && typeof val === 'number' && isFinite(val))
@@ -24,64 +9,27 @@ const isNumber = val => (!isNaN(val) && typeof val === 'number' && isFinite(val)
 const roundNum = (num, fixed) => {
   let re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
   return Math.round(parseFloat(num.toString().match(re)[0]) * 10) / 10
-}
+};
 
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case 'INCREMENT': {
+    case 'increments/SET_GOLD_BASE': {
       return {
         ...state,
-        goldBase: isNumber(action.goldBase) ? roundNum(state.goldBase + action.goldBase, 2) : state.goldBase,
-        coalBase: isNumber(action.coalBase) ? roundNum(state.coalBase + action.coalBase, 2) : state.coalBase,
-        ironBase: isNumber(action.ironBase) ? roundNum(state.ironBase + action.ironBase, 2) : state.ironBase,
-        stoneBase: isNumber(action.stoneBase) ? roundNum(state.stoneBase + action.stoneBase, 2) : state.stoneBase,
-        woodBase: isNumber(action.woodBase) ? roundNum(state.woodBase + action.woodBase, 2) : state.woodBase,
-        foodBase: isNumber(action.foodBase) ? roundNum(state.foodBase + action.foodBase, 2) : state.foodBase
+        goldBase: action.goldBase
       }
     }
-    case 'DECREMENT': {
+    case 'increments/SET_MULTIPLIER': {
       return {
         ...state,
-        goldBase: isNumber(action.goldBase) ? roundNum(state.goldBase - action.goldBase, 2) : state.goldBase,
-        coalBase: isNumber(action.coalBase) ? roundNum(state.coalBase - action.coalBase, 2) : state.coalBase,
-        ironBase: isNumber(action.ironBase) ? roundNum(state.ironBase - action.ironBase, 2) : state.ironBase,
-        stoneBase: isNumber(action.stoneBase) ? roundNum(state.stoneBase - action.stoneBase, 2) : state.stoneBase,
-        woodBase: isNumber(action.woodBase) ? roundNum(state.woodBase - action.woodBase, 2) : state.woodBase,
-        foodBase: isNumber(action.foodBase) ? roundNum(state.foodBase - action.foodBase, 2) : state.foodBase
+        goldMulti: action.goldMulti
       }
     }
-    case 'INCREASE_MULTIPLIER': {
-      return {
-        ...state,
-        goldMulti: isNumber(action.goldMulti) ? roundNum(state.goldMulti + action.goldMulti, 2) : state.goldMulti,
-        coalMulti: isNumber(action.coalMulti) ? roundNum(state.coalMulti + action.coalMulti, 2) : state.coalMulti,
-        ironMulti: isNumber(action.ironMulti) ? roundNum(state.ironMulti + action.ironMulti, 2) : state.ironMulti,
-        stoneMulti: isNumber(action.stoneMulti) ? roundNum(state.stoneMulti + action.stoneMulti, 2) : state.stoneMulti,
-        woodMulti: isNumber(action.woodMulti) ? roundNum(state.woodMulti + action.woodMulti, 2) : state.woodMulti,
-        foodMulti: isNumber(action.foodMulti) ? roundNum(state.foodMulti + action.foodMulti, 2) : state.foodMulti
-      }
-    }
-    case 'DECREASE_MULTIPLIER': {
-      return {
-        ...state,
-        goldMulti: isNumber(action.goldMulti) ? roundNum(state.goldMulti - action.goldMulti, 2) : state.goldMulti,
-        coalMulti: isNumber(action.coalMulti) ? roundNum(state.coalMulti - action.coalMulti, 2) : state.coalMulti,
-        ironMulti: isNumber(action.ironMulti) ? roundNum(state.ironMulti - action.ironMulti, 2) : state.ironMulti,
-        stoneMulti: isNumber(action.stoneMulti) ? roundNum(state.stoneMulti - action.stoneMulti, 2) : state.stoneMulti,
-        woodMulti: isNumber(action.woodMulti) ? roundNum(state.woodMulti - action.woodMulti, 2) : state.woodMulti,
-        foodMulti: isNumber(action.foodMulti) ? roundNum(state.foodMulti - action.foodMulti, 2) : state.foodMulti
-      }
-    }
-    case 'CALCULATE_INCREMENTS': {
+    case 'increments/CALCULATE_INCREMENTS': {
       return {
         ...state,
         goldIncr: Math.floor((state.goldBase * state.goldMulti) * 10) / 10,
-        coalIncr: Math.floor((state.coalBase * state.coalMulti) * 10) / 10,
-        ironIncr: Math.floor((state.ironBase * state.ironMulti) * 10) / 10,
-        stoneIncr: Math.floor((state.stoneBase * state.stoneMulti) * 10) / 10,
-        woodIncr: Math.floor((state.woodBase * state.woodMulti) * 10) / 10,
-        foodIncr: Math.floor((state.foodBase * state.foodMulti) * 10) / 10
       }
     }
     default: {
--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,10 +1,12 @@
 import resources from './parts/resources'
 import units from './parts/units'
 import buildings from './parts/buildings'
+import increments from './parts/increments'
 
 export default (state = {}, action) => {
   return {
     resources: resources(state.resources, action),
+    increments: increments(state.increments, action),
     units: units(state.units, action),
     buildings: buildings(state.buildings, action)
   }
