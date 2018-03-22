a9168d115550e137c2d2eeb5e1f45303063692db
Artur Falborski <falborski.artur@gmail.com>
2018-03-22 18:16:46 +0100

    increment gold per second in the game

--- src/ResourcesView.js
+++ src/ResourcesView.js
@@ -5,11 +5,57 @@ import './ResourcesView.css'
 
 let mapStateToProps = state => {
   return {
-    resources: state.resources
+    resources: state.resources,
+    increments: state.increments,
+    units: state.units
   }
 };
 
+let mapDipsatchToProps = dispatch => {
+  return {
+    setGoldBase: goldBase => dispatch({type: "increments/SET_GOLD_BASE", goldBase}),
+    calcIncr: () => dispatch({type: "increments/CALCULATE_INCREMENTS"}),
+    incrementGold: gold => dispatch({type: "resources/INCREMENT", gold})
+  }
+}
+
 class ResourcesView extends Component {
+
+  componentDidMount() {
+    this.setState({
+      setGoldBaseInt: setInterval(() => {
+        this.setGoldBase();
+        this.props.calcIncr();
+      }, 1000),
+
+      incrementGold: setInterval(() => {
+        this.props.incrementGold(this.props.increments.goldIncr)
+      }, 1000)
+    })
+  }
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+
+
+  setGoldBase = () => {
+    let result = 0;
+
+    for (let unit in this.props.units.data) {
+      if (this.props.units.data.hasOwnProperty(unit)) {
+        result += this.props.units.data[unit].quantity * this.props.units.data[unit].efficiency;
+      }
+    }
+
+    this.props.setGoldBase(result);
+    return null;
+  };
+
   render() {
     return (
       <div>
@@ -20,4 +66,4 @@ class ResourcesView extends Component {
   }
 }
 
-export default connect(mapStateToProps)(ResourcesView)
+export default connect(mapStateToProps, mapDipsatchToProps)(ResourcesView)
