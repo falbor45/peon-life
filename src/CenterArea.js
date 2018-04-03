e82efa5f18070b89243c8c6e61b66f52e98bac68
Artur Falborski <falborski.artur@gmail.com>
2018-04-03 07:48:34 +0200

    add ticks per second info to CenterArea component

--- src/CenterArea.js
+++ src/CenterArea.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import 'normalize.css'
+import './CenterArea.css'
 
 let mapStateToProps = state => {
   return {
@@ -18,14 +19,18 @@ let mapDispatchToProps = dispatch => {
 class CenterArea extends Component {
   render() {
     return (
-      <div style={{width: '33.33%', display: 'inline-block'}}>
+      <div className="center-area">
         <p>
-          <span style={{fontWeight: 'bold'}}>Total units: </span>
+          <span className="center-area__info">Total units: </span>
           <span>{this.props.units.units}</span>
         </p>
         <p>
-          <span style={{fontWeight: 'bold'}}>Gold per second: </span>
+          <span className="center-area__info">Gold per second: </span>
           <span>{this.props.increments.goldIncr}</span>
+        </p>
+        <p>
+          <span className="center-area__info">Ticks per second: </span>
+          <span>{this.props.increments.ticksPerSec}</span>
         </p>
       </div>
     )
