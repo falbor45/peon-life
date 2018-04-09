c5e787d4f8f074b53b58c16a354ec5a5a1668a98
Artur Falborski <falborski.artur@gmail.com>
2018-04-09 20:02:10 +0200

    hold totalGold value in resources reducer

--- src/state/parts/resources.js
+++ src/state/parts/resources.js
@@ -1,6 +1,7 @@
 const initialState = {
   gold: 0,
-  trueGold: 0
+  trueGold: 0,
+  totalGold: 0
 }
 
 const isNumber = val => (!isNaN(val) && typeof val === 'number' && isFinite(val))
@@ -9,15 +10,19 @@ const isNumber = val => (!isNaN(val) && typeof val === 'number' && isFinite(val)
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'resources/INCREMENT': {
-      let trueGold =  isNumber(action.gold) ? state.trueGold + action.gold : state.trueGold
+      let trueGold =  isNumber(action.gold) ? state.trueGold + action.gold : state.trueGold;
+      let totalGold = isNumber(action.gold) ? state.totalGold + action.gold : state.totalGold;
       return {
+        ...state,
         gold: Math.floor(trueGold),
-        trueGold: trueGold
+        trueGold: trueGold,
+        totalGold: totalGold
       }
     }
     case 'resources/DECREMENT': {
       let trueGold =  isNumber(action.gold) ? state.trueGold - action.gold : state.trueGold
       return {
+        ...state,
         gold: Math.floor(trueGold),
         trueGold: trueGold
       }
