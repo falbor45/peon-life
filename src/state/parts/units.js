a570261be28ef4ea442b40aded716441e49bff62
Artur Falborski <falborski.artur@gmail.com>
2018-05-03 19:18:05 +0200

    implemented buying in bulk in units reducer

--- src/state/parts/units.js
+++ src/state/parts/units.js
@@ -6,6 +6,22 @@ const initialState = {
   data: null,
   unitLimit: new BigNumber(5),
   units: new BigNumber(0),
+  buyQuant: new BigNumber(1)
+}
+
+let pow = (n, m) => {
+  let result = new BigNumber(1);
+  for (let i = 0; i < m; i++) {
+    result = result.multipliedBy(n);
+  }
+  return result;
+};
+
+let calculatePurchaseCost = (costBase, costMultiplier, owned, toBuy) => {
+  if (toBuy === 1) {
+    return costBase.multipliedBy(pow(costMultiplier, owned))
+  }
+  return costBase.multipliedBy((pow(costMultiplier, owned).multipliedBy(pow(costMultiplier, toBuy).minus('1'))).dividedBy(costMultiplier.minus('1')))
 }
 
 export default (state = initialState, action) => {
@@ -37,12 +53,11 @@ export default (state = initialState, action) => {
       }
     }
     case 'units/ADD_WORKER': {
-      let value = new BigNumber(action.value);
-      state.data[action.worker].quantity = state.data[action.worker].quantity.plus(value);
-      state.data[action.worker].cost.combined = state.data[action.worker].cost.combined.multipliedBy(state.data[action.worker].cost.multiplier);
+      state.data[action.worker].quantity = state.data[action.worker].quantity.plus(state.buyQuant);
+      state.data[action.worker].cost.combined = calculatePurchaseCost(state.data[action.worker].cost.base, state.data[action.worker].cost.multiplier, state.data[action.worker].quantity, state.buyQuant);
       return {
         ...state,
-        units: state.units.plus(value).isLessThanOrEqualTo(state.unitLimit) ? state.units.plus(value) : state.units
+        units: state.units.plus(state.buyQuant).isLessThanOrEqualTo(state.unitLimit) ? state.units.plus(state.buyQuant) : state.units
       }
     }
     case 'units/INCREASE_WORKER_EFFICIENCY': {
@@ -61,6 +76,16 @@ export default (state = initialState, action) => {
       state.data[action.worker].unlocked = true;
       return {
         ...state
+      }
+    }
+    case 'units/CHANGE_BUY_QUANT': {
+      let value = new BigNumber(action.value);
+      Object.keys(state.data).forEach(unit => {
+        state.data[unit].cost.combined = calculatePurchaseCost(state.data[unit].cost.base, state.data[unit].cost.multiplier, state.data[unit].quantity, value);
+      });
+      return {
+        ...state,
+        buyQuant: value
       }
     }
     default:
