fab9fe96298006a6089c00679ee59a1cf60c270a
Artur Falborski <falborski.artur@gmail.com>
2018-05-05 16:48:33 +0200

    display current game date in CenterArea component

--- src/CenterArea.js
+++ src/CenterArea.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import { BigNumber } from 'bignumber.js'
 import 'normalize.css'
 import './CenterArea.css'
 
@@ -8,7 +9,8 @@ let mapStateToProps = state => {
     resources: state.resources,
     units: state.units,
     increments: state.increments,
-    happiness: state.happiness
+    happiness: state.happiness,
+    time: state.time
   }
 };
 
@@ -21,13 +23,16 @@ class CenterArea extends Component {
   render() {
     return (
       <div className="center-area">
+        <p className="center-area__info">
+          <span>Year {this.props.time.date.year.toString()} Month {this.props.time.date.month.toString()} Day {this.props.time.date.day.toString()}</span>
+        </p>
         <p>
           <span className="center-area__info">Total units: </span>
           <span>{this.props.units.units.toString()} / {this.props.units.unitLimit.toString()}</span>
         </p>
         <p>
-          <span className="center-area__info">Ticks per second: </span>
-          <span>{this.props.increments.ticksPerSec.toString()}</span>
+          <span className="center-area__info">Hour duration: </span>
+          <span>{this.props.time.hourDuration.dividedBy(1000).decimalPlaces(2).toString()}s</span>
         </p>
         <p>
           <span className="center-area__info">Happiness: </span>
