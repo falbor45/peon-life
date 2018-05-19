94dca4bc554959431a8a282e7533cd262e8dbfb7
Artur Falborski <falborski.artur@gmail.com>
2018-05-19 19:14:02 +0200

    begin in-game time from year/month/day === 0/0/0

--- src/state/parts/time.js
+++ src/state/parts/time.js
@@ -3,28 +3,28 @@ import { BigNumber } from 'bignumber.js'
 const initialState = {
   hourDuration: new BigNumber(1000),
   date: {
-    year: new BigNumber(1),
-    month: new BigNumber(1),
-    day: new BigNumber(1),
+    year: new BigNumber(0),
+    month: new BigNumber(0),
+    day: new BigNumber(0),
     hour: new BigNumber(12)
   }
 };
 
 let moveDate = date => {
   date.hour = date.hour.plus(1);
-  if (date.hour > 24) {
+  if (date.hour > 23) {
     date.day = date.day.plus(1);
-    date.hour = new BigNumber(1);
+    date.hour = new BigNumber(0);
     // date.day += 1
     // move hour = 1
-    if (date.day > 30) {
+    if (date.day > 29) {
       date.month = date.month.plus(1);
-      date.day = new BigNumber(1);
+      date.day = new BigNumber(0);
       // date.month += 1
       // move day = 1
-      if (date.month > 10) {
+      if (date.month > 9) {
         date.year = date.year.plus(1);
-        date.month = new BigNumber(1);
+        date.month = new BigNumber(0);
         // date.year += 1
         // move month = 1
       }
